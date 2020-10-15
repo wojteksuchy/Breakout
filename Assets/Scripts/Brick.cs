d7a533f6e9e72119ba4fe1ffee2a6222fb667d7a
Wojciech <wsuchy@gmail.com>
2020-10-15 17:19:28 +0100

    Add Brick

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public delegate void OnBrickDestruction(Brick brick);
+    public static event OnBrickDestruction BrickDestroyed;
+    //public static event Action<Brick> BrickDestroyed;
+
+    public int HitPoint = 1;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        CollisionLogic(ball);
+    }
+
+    private void CollisionLogic(Ball ball)
+    {
+        HitPoint--;
+        if (HitPoint <=0)
+        {
+            BrickDestroyed?.Invoke(this);
+            //TODO: Add destroy effect
+            Destroy(gameObject);
+        }
+        else
+        {
+            //TODO: Change visual
+        }
+    }
+}
